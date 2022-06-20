192dabbd6d7b6d10d3955ef5dd03f4cc3df1b36e
Jason Washburn <jason.washburn@gmail.com>
2022-06-20 17:41:52 -0500

    adding db functionaility, partial complete

--- src/weatherscore/scraping/wowt.py
+++ src/weatherscore/scraping/wowt.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Dict, List
 
 import pandas as pd
 import pendulum
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from pendulum.datetime import DateTime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from sqlalchemy import create_engine
 
 WOWT_URL = "https://www.wowt.com/weather"
 
@@ -21,12 +22,16 @@ def get_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
     driver = webdriver.Chrome(options=chrome_options)
 
     return driver
 
 
-def parse_page_source(page_source) -> list[dict[str, Union[str, int]]]:
+def parse_page_source(page_source) -> List[Dict[str, Union[str, int]]]:
     soup = BeautifulSoup(page_source, "html.parser")
 
     hourly_row_divs = soup.find_all(name="div", attrs="hour")
@@ -66,9 +71,31 @@ def main():
     df["valid_time"] = pd.to_datetime(df["valid_time"])
     df["recorded_time"] = pd.to_datetime(df["recorded_time"])
     print(df)
-    records = df.to_json(orient="records")
-    print("----")
-    print(records)
+
+    with get_db_conn() as conn:
+        write_df_to_db(df=df, table="hourly_forecasts", conn=conn)
+        read_from_db(conn=conn, table="hourly_forecasts")
+
+
+def get_db_conn():
+    conn_string = "postgresql://airflow:@1rfl0w@postgres/scrapes"
+
+    db = create_engine(conn_string)
+    conn = db.connect()
+    return conn
+
+
+def write_df_to_db(df: pd.DataFrame, table: str, conn):
+    df.to_sql(table, con=conn, if_exists="replace", index=False)
+
+
+def read_from_db(conn, table: str):
+    cursor = conn.cursor()
+
+    sql1 = f"""select * from {table};"""
+    cursor.execute(sql1)
+    for i in cursor.fetchall():
+        print(i)
 
 
 if __name__ == "__main__":
